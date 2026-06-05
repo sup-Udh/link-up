8254a77a108ac270eb77e5bf400ee7d45abd9989
udhay rajeev <57910021+sup-Udh@users.noreply.github.com>
2026-06-05 21:15:09 +0530

    rebuild-langae support

--- app/lib/languages.ts
+++ app/lib/languages.ts
@@ -11,16 +11,6 @@ export const SUPPORTED_LANGUAGES: LanguageConfig[] = [
   { id: "python", name: "Python", monacoLanguage: "python", wandboxCompiler: "cpython-3.12.7" },
   { id: "java", name: "Java", monacoLanguage: "java", wandboxCompiler: "openjdk-jdk-22+36" },
   { id: "cpp", name: "C++", monacoLanguage: "cpp", wandboxCompiler: "gcc-13.2.0" },
-  { id: "c", name: "C", monacoLanguage: "c", wandboxCompiler: "gcc-13.2.0" },
-  { id: "csharp", name: "C#", monacoLanguage: "csharp", wandboxCompiler: "dotnetcore-8.0.402" },
-  { id: "go", name: "Go", monacoLanguage: "go", wandboxCompiler: "go-1.23.2" },
-  { id: "rust", name: "Rust", monacoLanguage: "rust", wandboxCompiler: "rust-1.82.0" },
-  { id: "kotlin", name: "Kotlin", monacoLanguage: "kotlin", wandboxCompiler: "openjdk-jdk-22+36" }, // Wandbox doesn't support Kotlin directly
-  { id: "swift", name: "Swift", monacoLanguage: "swift", wandboxCompiler: "swift-6.0.1" },
-  { id: "php", name: "PHP", monacoLanguage: "php", wandboxCompiler: "php-8.3.12" },
-  { id: "ruby", name: "Ruby", monacoLanguage: "ruby", wandboxCompiler: "ruby-3.4.9" },
-  { id: "scala", name: "Scala", monacoLanguage: "scala", wandboxCompiler: "scala-3.5.1" },
-  { id: "dart", name: "Dart", monacoLanguage: "dart", wandboxCompiler: "nodejs-20.17.0" }, // Wandbox doesn't support Dart directly
 ];
 
 export function getLanguageConfig(id: string): LanguageConfig {
