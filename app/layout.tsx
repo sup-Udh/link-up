bea356d6c967299fb08fde54ea1e7c2e9df84c76
udhay rajeev <57910021+sup-Udh@users.noreply.github.com>
2026-06-12 14:04:39 +0530

    SCRIPT!

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "./components/ThemeProvider";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Linko",
@@ -22,6 +23,27 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           {children}
         </ThemeProvider>
+
+        <Script id="x-pixel" strategy="afterInteractive">
+          {`
+            !function(e,t,n,s,u,a){
+              e.twq||(s=e.twq=function(){
+                s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+              },
+              s.version='1.1',
+              s.queue=[],
+              u=t.createElement(n),
+              u.async=!0,
+              u.src='https://static.ads-twitter.com/uwt.js',
+              a=t.getElementsByTagName(n)[0],
+              a.parentNode.insertBefore(u,a))
+            }(window,document,'script');
+
+            twq('config','rcywr');
+            twq('track','PageView');
+          `}
+        </Script>
+
       </body>
     </html>
   );
